2a7bc84025c1b6232f1a018758a276de1de0c5df
Gabriel Assana <gabrielhhz2@gmail.com>
2022-05-26 22:27:22 -0300

    selecionando elementos da tela

---
+++ js/elements.js
@@ -0,0 +1,25 @@
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonIncrementer = document.querySelector('.increase')
+const buttonDecrementer = document.querySelector('.decrease')
+
+const buttonForest = document.querySelector('.forest')
+const buttonRain = document.querySelector('.rain')
+const buttonCoffeeShop = document.querySelector('.coffeeShop')
+const buttonFireplace = document.querySelector('.fireplace')
+
+export {
+  minutesDisplay,
+  secondsDisplay,
+  buttonPlay,
+  buttonStop,
+  buttonIncrementer,
+  buttonDecrementer,
+  buttonForest,
+  buttonRain,
+  buttonCoffeeShop,
+  buttonFireplace
+}
